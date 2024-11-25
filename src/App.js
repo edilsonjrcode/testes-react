55b49afe97d20a4733a99a7fb08a1534ea8e1e69
edilsonjrcode <2022111510116@iesp.edu.br>
2024-11-25 00:36:19 -0300

    fix: insere css e atividade 2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import './App.css';
 import LoginForm from './components/Form/LoginForm';
+import { ThemeProvider, useTheme } from './hooks/ThemeContext';
+
+const ThemeToggler = () => {
+  const { theme, toggleTheme } = useTheme();
+
+  return (
+    <div className={`app-container ${theme}`}>
+      <h1>{theme === 'light' ? 'Tema Claro' : 'Tema Escuro'}</h1>
+      <button onClick={toggleTheme}>
+        Alterar para {theme === 'light' ? 'Tema Escuro' : 'Tema Claro'}
+      </button>
+    </div>
+  );
+};
+
 
 function App() {
   return (
-    <div className="App">
+    <ThemeProvider>
+      <ThemeToggler />
       <LoginForm/>
-    </div>
+    </ThemeProvider>
+     
   );
 }
 
